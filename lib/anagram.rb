9f32052165510f18075fd4810253fb705632a132
Jaskiran Singh <jsingh8986@yahoo.com>
2023-01-31 18:30:12 -0800

    commit tree c3a052b8423eda2e5d355381ec9e70aeafd70533

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @name = word
+    end
+
+    def match(ana_array)
+        ana_array.select {|anagram| anagram.split('').sort == @name.split('').sort}
+        # take the word and split it into letters
+        # take the array of letters and compare 
+        # them to each word of the anagram
+    
+
+    end
+
+end
